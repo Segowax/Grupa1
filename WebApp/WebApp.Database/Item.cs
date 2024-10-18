512d998ba0c1769f730c8712857e5fbc08556542
Uczen <Uczen@DESKTOP-FE0UD96>
2024-10-18 12:14:18 +0200

    zadanie 1,2,3

---
+++ WebApp/WebApp.Database/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Database
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(int id, Guid guid, string name, int quantity)
+        {
+            Id = id;
+            Guid = guid;
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
+
