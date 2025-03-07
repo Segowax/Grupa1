dbd011c1355062a6c80be5e8fc07e7c24e090d88
Uczen <uczen@dzdzw.local>
2025-03-07 12:22:26 +0100

    Dodano zmiany 07.03

--- WebApp/WebApp.Client/Program.cs
+++ WebApp/WebApp.Client/Program.cs
@@ -6,9 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri("https://my-wonderful-api-acgvdvaaa5d4b9ez.northeurope-01.azurewebsites.net/api/")
+builder.Services.AddHttpClient("nieWiem", opt => {
+    opt.BaseAddress = new Uri("https://my-wonderful-api-acgvdvaaa5d4b9ez.northeurope-01.azurewebsites.net");
+});
+builder.Services.AddHttpClient("api", opt => {
+    opt.BaseAddress = new Uri("https://localhost:7071");
 });
 
 await builder.Build().RunAsync();
